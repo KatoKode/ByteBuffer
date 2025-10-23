ca6666c60432b5ce245bf5e9e9a42356cf6b7c56
Data Structures and Algorithms in C and Assembly Language <mcintosh1798@gmail.com>
2025-10-22 20:47:32 -0500

    Add files via upload

--- demo/main.c
+++ demo/main.c
@@ -27,6 +27,8 @@ int main (int argc, char **argv)
 
   bb_init(buffer, BUFFER_SIZE, NULL);
 
+  getBufferMeta(buffer);
+
   putValues(buffer);
 
   bb_flip(buffer);
@@ -51,6 +53,24 @@ void getNativeByteOrder()
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
+void getBufferMeta (bytebuffer_t *buffer)
+{
+  byte_t *bbuffer = bb_get_buffer(buffer);
+  printf ("bb_get_buffer: %p\n", bbuffer);
+  size_t bound = bb_get_bound(buffer);
+  printf ("bb_get_bound: %lu\n", bound);
+  size_t index = bb_get_index(buffer);
+  printf ("bb_get_index: %lu\n", index);
+  bool_t flag = bb_has_more(buffer);
+  printf ("bb_has_more: %hu\n", flag);
+  size_t remaining = bb_get_remaining(buffer);
+  printf ("bb_remaining: %lu\n", remaining);
+  size_t size = bb_get_size(buffer);
+  printf ("bb_get_size: %lu\n", size);
+
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
 double getDouble (bytebuffer_t *buffer)
 {
   (void) bb_get(buffer);
@@ -59,11 +79,27 @@ double getDouble (bytebuffer_t *buffer)
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
-float getfloat (bytebuffer_t *buffer)
+double getDoubleAt (bytebuffer_t *buffer, size_t index)
+{
+  (void) bb_get_at(buffer, index);
+
+  return bb_get_double_at(buffer, index + 1);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+float getFloat (bytebuffer_t *buffer)
 {
   (void) bb_get(buffer);
 
   return bb_get_float(buffer);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+float getFloatAt (bytebuffer_t *buffer, size_t index)
+{
+  (void) bb_get_at(buffer, index);
+
+  return bb_get_float_at(buffer, index + 1);
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
@@ -75,11 +111,27 @@ int16_t getI16 (bytebuffer_t *buffer)
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
+int16_t getI16At (bytebuffer_t *buffer, size_t index)
+{
+  (void) bb_get_at(buffer, index);
+
+  return bb_get_int16_at(buffer, index + 1);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
 int32_t getI32 (bytebuffer_t *buffer)
 {
   (void) bb_get(buffer);
 
   return bb_get_int32(buffer);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+int32_t getI32At (bytebuffer_t *buffer, size_t index)
+{
+  (void) bb_get_at(buffer, index);
+
+  return bb_get_int32_at(buffer, index + 1);
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
@@ -91,6 +143,14 @@ int64_t getI64 (bytebuffer_t *buffer)
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
+int64_t getI64At (bytebuffer_t *buffer, size_t index)
+{
+  (void) bb_get_at(buffer, index);
+
+  return bb_get_int64_at(buffer, index + 1);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
 char * getText (bytebuffer_t *buffer)
 {
   size_t n = bb_get(buffer);
@@ -98,12 +158,32 @@ char * getText (bytebuffer_t *buffer)
   return bb_get_varchar(buffer, n);
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+char * getTextAt (bytebuffer_t *buffer, size_t index)
+{
+  size_t n = bb_get_at(buffer, index);
+
+  return bb_get_varchar_at(buffer, n, index + 1);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // GETVALUES
 void getValues (bytebuffer_t *buffer)
 {
+  // getText
+
+  size_t index = bb_get_index(buffer);
+
   char *text = getText(buffer);
 
   printf("text: %s\n", text);
+
+  free(text);
+
+  // getTextAt
+
+  text = getTextAt(buffer, index);
+
+  printf("text: %s at: %lu\n", text, index);
 
   free(text);
 
@@ -113,13 +193,27 @@ void getValues (bytebuffer_t *buffer)
     byte_t b[8];
   } u;
 
+  // getDouble
+
+  index = bb_get_index(buffer);
+
   u.dbl = getDouble(buffer);
+
+  printf("double: %le\n", u.dbl);
 #ifdef BB_DEBUG
-  printf("hex bytes of a double: ");
+  printf("DEBUG: %s: ", __func__);
   for (int i = 0; i < 8; ++i) printf("%02X, ", u.b[i]);
-  printf("== %1.11lf\n", u.dbl);
+  printf("== %le\n", u.dbl);
 #endif
-  printf("double: %1.11lf\n", u.dbl);
+  // getDoubleAt
+
+  u.dbl = getDoubleAt(buffer, index);
+
+  printf("double: %le at: %lu\n", u.dbl, index);
+
+  // getText
+
+  index = bb_get_index(buffer);
 
   text = getText(buffer);
 
@@ -127,39 +221,125 @@ void getValues (bytebuffer_t *buffer)
 
   free(text);
 
-  float flt = getfloat(buffer);
+  // getTextAt
 
-  printf("float: %f\n", flt);
+  text = getTextAt(buffer, index);
+
+  printf("text: %s at: %lu\n", text, index);
+
+  free(text);
+
+  // getFloat
+
+  index = bb_get_index(buffer);
+
+  float flt = getFloat(buffer);
+
+  printf("float: %e\n", flt);
+
+  // getFloatAt
+
+  flt = getFloatAt(buffer, index);
+
+  printf("float: %e at: %lu\n", flt, index);
+
+  // getText
+
+  index = bb_get_index(buffer);
 
   text = getText(buffer);
 
   printf("text: %s\n", text);
 
   free(text);
+
+  // getTextAt
+
+  text = getTextAt(buffer, index);
+
+  printf("text: %s at: %lu\n", text, index);
+
+  free(text);
+
+  // getI64
+
+  index = bb_get_index(buffer);
 
   int64_t i64 = getI64(buffer);
 
   printf("int64_t: %ld\n", i64);
 
+  // getI64At
+
+  i64 = getI64At(buffer, index);
+
+  printf("int64_t: %ld at: %lu\n", i64, index);
+
+  // getText
+
+  index = bb_get_index(buffer);
+
   text = getText(buffer);
 
   printf("text: %s\n", text);
 
   free(text);
+
+  // getTextAt
+
+  text = getTextAt(buffer, index);
+
+  printf("text: %s at: %lu\n", text, index);
+
+  free(text);
+
+  // getI16
+
+  index = bb_get_index(buffer);
 
   int16_t i16 = getI16(buffer);
 
   printf("int16_t: %hd\n", i16);
 
+  // getI16At
+
+  i16 = getI16At(buffer, index);
+
+  printf("int16_t: %hd at: %lu\n", i16, index);
+
+  // getText
+
+  index = bb_get_index(buffer);
+
   text = getText(buffer);
 
   printf("text: %s\n", text);
 
   free(text);
 
+  // getTextAt
+
+  text = getTextAt(buffer, index);
+
+  printf("text: %s at: %lu\n", text, index);
+
+  free(text);
+
+  // getI32
+
+  index = bb_get_index(buffer);
+
   int32_t i32 = getI32(buffer);
 
   printf("int32_t: %d\n", i32);
+
+  // getI32At
+
+  index = bb_get_index(buffer);
+
+  i32 = getI32At(buffer, index);
+
+  printf("int32_t: %d at: %lu\n", i32, index);
 
   for (int i = 0; i < 10; ++i)
   {
@@ -173,14 +353,31 @@ void getValues (bytebuffer_t *buffer)
 void putDouble (bytebuffer_t *buffer, double const value)
 {
   bb_put(buffer, (byte_t)8);
-
+#ifdef BB_DEBUG
+  size_t index = bb_get_index(buffer);
+#endif
   bb_put_double(buffer, value);
 #ifdef BB_DEBUG
   printf("DEBUG: %s: ", __func__);
-  for (int i = 6; i < 14; ++i)
+  for (int i = index; i < index + 8; ++i)
     printf("%02X, ", buffer->buffer[i]);
   putchar('\n');
-  printf("DEBUG: %1.11lf\n",*(double*)(&buffer->buffer[6]));
+  printf("DEBUG: %le == %le\n",*(double*)(&buffer->buffer[index]), value);
+#endif
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+void putDoubleAt (bytebuffer_t *buffer, size_t index, double const value)
+{
+  bb_put_at(buffer, index, (byte_t)8);
+
+  bb_put_double_at(buffer, index + 1, value);
+#ifdef BB_DEBUG
+  printf("DEBUG: %s: ", __func__);
+  for (int i = index + 1; i < index + 9; ++i)
+    printf("%02X, ", buffer->buffer[i]);
+  putchar('\n');
+  printf("DEBUG: %le == %le\n",*(double*)(&buffer->buffer[index + 1]), value);
 #endif
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -193,11 +390,27 @@ void putFloat (bytebuffer_t *buffer, float const value)
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
+void putFloatAt (bytebuffer_t *buffer, size_t index, float const value)
+{
+  bb_put_at(buffer, index, (byte_t)4);
+
+  bb_put_float_at(buffer, index + 1, value);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
 void putI16 (bytebuffer_t *buffer, int16_t const value)
 {
   bb_put(buffer, (byte_t)2);
 
   bb_put_int16(buffer, value);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+void putI16At (bytebuffer_t *buffer, size_t index, int16_t const value)
+{
+  bb_put_at(buffer, index, (byte_t)2);
+
+  bb_put_int16_at(buffer, index + 1, value);
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
@@ -209,11 +422,27 @@ void putI32 (bytebuffer_t *buffer, int32_t const value)
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
+void putI32At (bytebuffer_t *buffer, size_t index, int32_t const value)
+{
+  bb_put_at(buffer, index, (byte_t)4);
+
+  bb_put_int32_at(buffer, index + 1, value);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
 void putI64 (bytebuffer_t *buffer, int64_t const value)
 {
   bb_put(buffer, (byte_t)8);
 
   bb_put_int64(buffer, value);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+void putI64At (bytebuffer_t *buffer, size_t index, int64_t const value)
+{
+  bb_put_at(buffer, index, (byte_t)8);
+
+  bb_put_int64_at(buffer, index + 1, value);
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //
@@ -224,6 +453,16 @@ void putText (bytebuffer_t *buffer, char const *text)
   bb_put(buffer, (byte_t)n);
 
   bb_put_varchar (buffer, text);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+void putTextAt (bytebuffer_t *buffer, size_t index, char const *text)
+{
+  size_t n = strlen(text);
+
+  bb_put_at(buffer, index, (byte_t)n);
+
+  bb_put_varchar_at(buffer, index + 1, text);
 }
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // PUTVALUES
@@ -238,12 +477,17 @@ void putValues (bytebuffer_t *buffer)
     byte_t b[8];
   } du;
 
-  du.dbl = 0.00009876543;
-  printf("%s: ", __func__);
+  du.dbl = DBL_MIN;
+  printf("DEBUG: %s: ", __func__);
   for (int i = 0; i < 8; ++i) printf("%02X, ", du.b[i]);
   putchar('\n');
+  printf("DEBUG: %le\n", du.dbl);
 #endif
-  putDouble(buffer, 0.00009876543);
+  size_t index = bb_get_index(buffer);
+
+  putDouble(buffer, DBL_MAX);
+
+  putDoubleAt(buffer, index, DBL_MIN);
 
   putText(buffer, "TEXT 1");
 
@@ -254,22 +498,43 @@ void putValues (bytebuffer_t *buffer)
   } fu;
 
   fu.flt = 1.05432;
+  printf("%s: hex bytes of float: ", __func__);
   for (int i = 0; i < 4; ++i) printf("%02X, ", fu.b[i]);
-  putchar('\n');
+  printf(" == %f\n", fu.flt);
 
-  putFloat(buffer, 1.05432);
+  index = bb_get_index(buffer);
+
+  putFloat(buffer, FLT_MAX);
+
+  putFloatAt(buffer, index, FLT_MIN);
 
   putText(buffer, "TEST 22");
 
-  putI64(buffer, 9876543210);
+  index = bb_get_index(buffer);
 
-  putText(buffer, "TEST 333");
+  putI64(buffer, LONG_MIN);
 
-  putI16(buffer, -32768);
+  putI64At(buffer, index, LONG_MAX);
+
+  index = bb_get_index(buffer);
+
+  putText(buffer, "TEST 777");
+
+  putTextAt(buffer, index, "TEST 333");
+
+  index = bb_get_index(buffer);
+
+  putI16(buffer, SHRT_MAX);
+
+  putI16At(buffer, index, SHRT_MIN);
 
   putText(buffer, "TEST 4444");
 
-  putI32(buffer, 131072);
+  index = bb_get_index(buffer);
+
+  putI32(buffer, INT_MIN);
+
+  putI32At(buffer, index, INT_MAX);
 
   for (int i = 0; i < 10; ++i)
   {
--- demo/main.h
+++ demo/main.h
@@ -25,25 +25,42 @@
 #include "../bytebuffer/bytebuffer.h"
 
 // To get debug output uncomment the following line and remake the demo program.
-//#define BB_DEBUG  1
+// #define BB_DEBUG  1
 
 #define BUFFER_SIZE     256
 
+#define INDEX_160   160
+#define INDEX_64    170
+
 void getNativeByteOrder();
 
+void getBufferMeta (bytebuffer_t *);
+
 double getDouble (bytebuffer_t *);
+double getDoubleAt (bytebuffer_t *, size_t);
 float getfloat (bytebuffer_t *);
+float getFloatAt (bytebuffer_t *, size_t);
 int16_t getI16 (bytebuffer_t *);
+int16_t getI16At (bytebuffer_t *, size_t);
 int32_t getI32 (bytebuffer_t *);
+int32_t getI32At (bytebuffer_t *, size_t);
 int64_t getI64 (bytebuffer_t *);
+int64_t getI64At (bytebuffer_t *, size_t);
 char * getText (bytebuffer_t *);
+char * getTextAt (bytebuffer_t *, size_t);
 
 void putDouble (bytebuffer_t *, double const);
+void putDoubleAt (bytebuffer_t *, size_t, double const);
 void putFloat (bytebuffer_t *, float const);
+void putFloatAt (bytebuffer_t *, size_t, float const);
 void putI16 (bytebuffer_t *, int16_t const);
+void putI16At (bytebuffer_t *, size_t, int16_t const);
 void putI32 (bytebuffer_t *, int32_t const);
+void putI32At (bytebuffer_t *, size_t, int32_t const);
 void putI64 (bytebuffer_t *, int64_t const);
+void putI64At (bytebuffer_t *, size_t, int64_t const);
 void putText (bytebuffer_t *, char const *);
+void putTextAt (bytebuffer_t *, size_t, char const *);
 
 void putValues (bytebuffer_t *);
 void getValues (bytebuffer_t *);
